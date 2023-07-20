77c985702386e8dffeb9c5bd139e6c1a3350b05e
nickferreti <nickrferreti6@gmail.com>
2023-07-19 21:08:29 -0300

    Dando os primeiros passos da aplicação

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,10 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Olá Mundo");
+        System.out.printf("Bem vindo ao Screen Match\n");
+        System.out.println("Filme: Top Gun Maverick");
+        int ano = 2022;
+        System.out.println("Ano de Lançamento: " + ano);
+        
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
     }
 }
