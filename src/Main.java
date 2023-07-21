1fdc89b42321a3bc19be669f484e33464f8b74b7
nickferreti <nickrferreti6@gmail.com>
2023-07-20 23:05:19 -0300

    Trabalhando com textos

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,23 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Bem vindo ao Screen Match\n");
-        System.out.println("Filme: Top Gun Maverick");
         int ano = 2022;
-        System.out.println("Ano de Lançamento: " + ano);
-        
+        boolean incluindoNoPlano = true;
+        double notaFilme = 8.1;
+        double notaMedia = (9.8 + 6.3 + 8.0) / 3;
+        String sinopse;
+        sinopse = """
+                Depois de mais de 30 anos de serviço como um dos 
+                principais aviadores da Marinha, Pete "Maverick" Mitchell 
+                está de volta, rompendo os limites como um piloto de 
+                testes corajoso. No mundo contemporâneo das guerras 
+                tecnológicas, Maverick enfrenta drones e prova que o 
+                fator humano ainda é essencial.
+                """;
 
+        System.out.printf("Bem vindo ao Screen Match\n");
+        System.out.println("Filme:\nTop Gun Maverick\n");
+        System.out.println("Ano de Lançamento:\n" + ano + "\n");
+        System.out.println("Media de nota do filme é:\n" + notaMedia + "\n");
+        System.out.println("Sinopse do Filme:\n" + sinopse);
     }
 }
